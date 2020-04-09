debdea917dc87f8b88f7fa75fc16b4d2c733c0c9
dizdonchik-png <56499507+dizdonchik-png@users.noreply.github.com>
2020-04-09 18:32:49 +0300

    Add files via upload

---
+++ arr.js
@@ -0,0 +1,12 @@
+VANTA.NET({
+  el: "#main",
+  mouseControls: true,
+  touchControls: true,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  points: 13.00,
+  maxDistance: 25.00,
+  spacing: 11.00
+})
